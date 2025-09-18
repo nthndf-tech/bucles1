62715c6545b37cf194be6d411268de7415c2d444
Natasha da Silva Fernandes <a25natashadsf@teis.intranet>
2025-09-18 20:30:23 +0200

    olo

---
+++ untitled/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = "Pepe";
+        System.out.println(decirlHola(name));
+    }
+
+    public static String decirlHola(String name) {
+        return "Hola "+ name;
+
+    }
+
+    public static void decirAdios(String n){
+        System.out.printf("Adios" + n);
+    }
+
+}
