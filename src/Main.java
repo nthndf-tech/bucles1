6df8661411910c23c3ba8d10e3efdb36f0a45b14
Natasha da Silva Fernandes <a25natashadsf@teis.intranet>
2025-09-19 19:52:13 +0200

    ate9

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+//public class Main {
+   // public static void main(String[] args) {
         //1. Contador simple (for): Ingresa un número N y muestra los números del 1 al N.
 
 //        Scanner sc = new Scanner(System.in);
@@ -79,15 +80,67 @@ public class Main {
         //7. Menú repetitivo (do-while): Muestra un menú simple con opciones hasta que el usuario elija
         //salir.
 
-        Scanner sc = new Scanner(System.in);
-        int eleccion = sc.nextInt();
+//        public class Main {
+//            public static void main(String[] args) {
+//
+//                Scanner sc = new Scanner(System.in);
+//                int eleccion;
+//                do {
+//                    System.out.println("    MENU    ");
+//                    System.out.println("1 - Entradas");
+//                    System.out.println("2 - Plato principal");
+//                    System.out.println("3 - Postre");
+//                    System.out.println("0 - Salir");
+//
+//
+//                    System.out.println("Elija una opción: ");
+//                    eleccion = sc.nextInt();
+//
+//
+//                } while (eleccion != 0);
+//            }
+//        }
 
-        do {
-            System.out.println("1 - Entradas");
-            System.out.println("2 - Plato principal");
-            System.out.println("3 - Postre");
-            System.out.println("0 - Salir");
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        //8. Suma hasta cero (do-while): Ingresa números y súmalos hasta que se ingrese un cero.
+//
+//        Scanner sc = new Scanner(System.in);
+//        int num;
+//        int soma = 0;
+//
+//        do {
+//            System.out.println("Ingrese el numero: ");
+//            num = sc.nextInt();
+//            System.out.println(soma += num);
+//
+//        } while (num != 0);
+//            System.out.println("Resultado: " + soma);
+//    }
+//
+//}
+        public class Main {
+            public static void main(String[] args) {
 
-        } while (eleccion != 0);
-    }
-}
+                //9. Validar contraseña (while): Pide una contraseña hasta que el usuario escriba la correcta.
+
+                Scanner sc = new Scanner(System.in);
+
+
+                String contrasenaValida = "1234@";
+                String contrasenaUsuario = "";
+                do {
+
+                }
+
+
+                while (contrasenaUsuario!=contrasenaValida){
+                    System.out.println("Contraseña:");
+                    System.out.println("Contraseña incorrecta");
+                }
+
+            }
+        }
+
+
